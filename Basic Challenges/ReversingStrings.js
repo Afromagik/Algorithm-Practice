0fe80dcfa26211b170593e326b5bdc54dd96d27d
Mosun Akinyemi <suzan.akinyemi101@gmail.com>
2020-05-11 21:34:33 +0200

    Adding comments

--- Basic Challenges/ReversingStrings.js
+++ Basic Challenges/ReversingStrings.js
@@ -1,21 +1,23 @@
 function reverseString(str) {
+    // default case
     if (str.length == 0 || str.length == 1) {
-      return str 
+      return str // return as is
     }
-    // turn into array
+    // split string into array
     const splitString = str.split("");
     console.log(splitString)
   
-    let reverseString = "" 
-    for (let count = 0; count <= splitString.length; count ++){ 
+    let reverseString = ""  //initalize return variable 
+    for (let count = 0; count <= splitString.length; count ++){ // positive loop through array
+        // handle undefined
       if (reverseString === "undefined"){
-        reverseString = (splitString[splitString.length - count])
-      } else { 
-        reverseString += (splitString[splitString.length - count])
+        reverseString = (splitString[splitString.length - count]) // replace undefined with last array character
+      } else { // normal case
+        reverseString += (splitString[splitString.length - count]) // append characters in reverse order
       }
     }
     console.log(reverseString)
     return reverseString
   }
   
-  reverseString("hello");
+  reverseString("hello"); // generic test
